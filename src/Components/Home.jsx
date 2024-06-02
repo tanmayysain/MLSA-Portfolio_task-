b381d2cadd641e700268cf470af18c0e96dc7feb
tanmayysain <tanmaysain02@gmail.com>
2024-06-03 04:18:25 +0530

    Added Changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/HeroImage.JPG";
+import image from "../images/heroImage.jpg";
 
 const imageAltText = "Apparently a photo of me singing in a band.";
 
